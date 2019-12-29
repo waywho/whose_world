9b98b981bc5256d7bbc595898922f0666b725c86
waywho <walzerfan@yahoo.com>
2019-12-29 09:35:53 +0000

    adding worker

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
+  	devise_for :users
     # api routes here
   end
-
+	
   root 'application#index'
   get '/*path', to: 'application#index' 
 end
